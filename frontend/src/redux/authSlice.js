7ba21258842f92a1852a654961d1bc4761ce0b14
Rebeca <mustelierrebeca99@gmail.com>
2026-03-17 12:57:05 -0500

    added auth

---
+++ frontend/src/redux/authSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  token: null,
+  isLoading: false,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { setLogin, setLogout, setLoading, setError } = authSlice.actions;
+export default authSlice.reducer;
--- frontend/src/routes/employee/route.jsx
+++ frontend/src/routes/employee/route.jsx
@@ -1,12 +1,22 @@
-import { Outlet, createFileRoute } from "@tanstack/react-router";
+import { Outlet, createFileRoute, useNavigate } from "@tanstack/react-router";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
+import { useEffect } from "react";
 
 export const Route = createFileRoute("/employee")({
   component: EmployeeLayoutComponent,
 });
 
 function EmployeeLayoutComponent() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate({ to: "/login" }); // Redirige al login si no hay token
+    }
+  }, []);
+  
   return (
     <div className="h-full relative">
       <SidebarProvider className="absolute left-0 top-0 h-full min-h-full">
