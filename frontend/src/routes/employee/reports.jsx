254ec9de8b010b08c80dd6f63edad92f20553508
Joshua L <joshua.j.lopsil@gmail.com>
2026-03-22 00:11:36 -0500

    What i need to add on reports minimum

--- frontend/src/routes/employee/reports.jsx
+++ frontend/src/routes/employee/reports.jsx
@@ -10,6 +10,11 @@ function RouteComponent() {
       <div>Reports Dashboard</div>
 
       <div style={{ marginTop: '20px' }}>
+        <div>Total Ingredients: </div>
+        <div>Total Employees: </div>
+        <div>Total Items on Menu: </div>
+        <div>Total Suppliers: </div>
+        <div>Total Trucks: </div>
         <div>Total Orders: </div>
         <div>Total Items Sold: </div>
         <div>Gross Income: $</div>
